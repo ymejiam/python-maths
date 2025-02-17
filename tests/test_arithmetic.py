668164a2af6e4276d1929c36d2b64bccd3d0375e
TCNG3 <simon96522@gmail.com>
2025-02-17 11:28:23 +0000

    Adds square_root() function and tests.
    hi

--- tests/test_arithmetic.py
+++ tests/test_arithmetic.py
@@ -69,3 +69,15 @@ def test_divide_zero_division_exception() -> None:
     """Test that a ZeroDivisionError is raised by the divide() function."""
     with pytest.raises(ZeroDivisionError):
         arithmetic.divide(2, 0)
+@pytest.mark.parametrize(
+    ("x", "target"),
+    [
+        pytest.mark(4, 2, id="square root of 4"),
+        pytest.mark(9, 3.0, id="square root of 9"),
+        pytest.mark(25, 5.0, id="square root of 25"),
+        pytest.mark(2, 1.4142135623730951, id="square root of 2"),
+    ],
+)
+def test_square_root(x: int | float, target: int | float) -> None:
+    """Test the square_root() function."""
+    assert pytest.approx(arithmetic.square_root(x), target)
